90ff260d8cfb6dd2cf1ba13eeccf9ebd16cb7b96
Allan Jardine <allan@datatables.net>
2022-10-10 19:48:09 +0000

    Add support for mjs files (ES Modules)

---
+++ js/autoFill.bootstrap4.min.mjs
@@ -0,0 +1,1 @@
+import $ from"jquery";import DataTable from"datatables.net-bs4";import AutoFill from"datatables.net-autofill";DataTable.AutoFill.classes.btn="btn btn-primary";export default DataTable;
