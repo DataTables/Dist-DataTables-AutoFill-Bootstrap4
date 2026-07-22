dd4b1517ddca78f5207a3b7ff9703d3e595a61d8
dtbuild <dtbuild@rigil>
2026-07-22 12:45:44 +0000

    a8c27eb2c9ad9bdff5ab6b633234a23be141f865 Release - 3.0.0
    Sync to source repo @a8c27eb2c9ad9bdff5ab6b633234a23be141f865

--- js/autoFill.bootstrap4.min.mjs
+++ js/autoFill.bootstrap4.min.mjs
@@ -1,4 +1,4 @@
-/*! AutoFill Bootstrap 4 styling 3.0.0-beta.1 for DataTables
+/*! AutoFill Bootstrap 4 styling 3.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-bs4";import AutoFill from"datatables.net-autofill";DataTable.AutoFill.classes.btn="btn btn-secondary";export default DataTable;
